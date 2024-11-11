21737fcdd9a5d755570d4ba9fc517bd186d5c66e
HOCINE Mohand Ouidir <hocinidir26@gmail.com>
2024-11-11 19:25:53 +0100

    create a file using the fs.open() method

--- Files_system/file_system.js
+++ Files_system/file_system.js
@@ -23,4 +23,14 @@ fs.appendFile('Files_system/mynewfile1.txt', 'Hello content!', function (err){
   console.log('Saved!');
 })
 
+// create a new file using the open() method
+// The fs.open() method takes a "flag" as the second argument, if the flag is "w" for "writing", the specified file is opened for writing. If the file does not exist, an empty file is created
+
+fs.open('Files_system/mynewfile2.txt', 'w', function (err, file){
+  if (err) throw err;
+  console.log('saved!');
+})
+
+
+
 
